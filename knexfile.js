dadc284f4b58a1ccaccfa8d2a6926c11b18f6fe5
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-07-27 18:19:46 -0400

    migrations/seeds init install

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,16 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/my_plants.db3'
     },
     migrations: {
       directory: "./data/migrations",
       tableName: "knex_migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     },
     useNullAsDefault: true
   },
