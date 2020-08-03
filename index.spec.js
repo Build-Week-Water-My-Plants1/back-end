0f38319bdba754effb1144ddc4a7a1f687a881bb
TatyanaDz <48102770+Tatyanadz@users.noreply.github.com>
2020-08-03 17:22:45 -0400

    tests updated

--- index.spec.js
+++ index.spec.js
@@ -1,156 +1,77 @@
-const server = require("./api/server")
+
 const supertest = require("supertest")
+const server = require("./api/server")
 const db = require("./data/db-config")
-const serverAPI = require("./api/server")
+const jwt = require("jsonwebtoken")
+const secret = require("./config/secrets")
+const Users = require("./users/users-model")
 
 
-require("dotenv").config()
+describe('server.js', () => {
+  beforeEach(async () => {
+    await db('users').truncate(); 
+  })
+ 
+  //REGISTER
 
-beforeEach(async () => {
-    await db.seed.run()
-})
-
-afterAll(async () => {
-    await db.destroy()
-})
-
-
-describe("test to check if API is running", () => {
-    it("GET /", async () => {
-        const res = await supertest(serverAPI).get("/")
-        expect(res.statusCode).toBe(200)
-        expect(res.body.message).toBe("Water My Plants App")
+  describe('POST /auth/register', () => {
+    it('add a user to the database', async () => {
+      const users = await db('users');
+      expect(users).toHaveLength(0);
+      await Users.add({
+        username: 'NewUser',
+        phone_number: '1234567891',
+        password: 'password',
+      })
+      const newUsers = await db('users');
+      expect(newUsers).toHaveLength(1);
     })
-
-    it("POST /register", async () => {
-        const res = await supertest(serverAPI).post("/api/auth/register").send({
-            username: "NewUser5",
-            phone_number: "1234",
-            password: "test",
-            
-        })
-        expect(res.statusCode).toBe(201)
-        expect(res.body).toBeDefined()
+    it('check added user', async () => {
+      const users = await db('users');
+      expect(users).toHaveLength(0);
+      await Users.add({
+        username: 'NewUser',
+        phone_number: '1234567891',
+        password: 'password'
+      })
     })
-
-    it("GET /api/auth/users", async () => {
-        const res = await supertest(serverAPI).get("/api/auth/users")
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toBeDefined();
-      });
-    
-    it("POST /api/auth/login", async () => { 
-       const res = await supertest(serverAPI).post("/api/auth/login").send({
-          username: "NewUser5",
-          password: "123abc"
-       });
-       expect(res.statusCode).toBe(201);
-       expect(res.body.message).toBe("Welcome NewUser5");
-    });       
-    
-    it("POST  & PUT /users/:id", async () => { 
-        const res1 = await supertest(serverAPI).post("/api/auth/login").send({
-          username: "NewUser5",
-          password: "123abc"
-        });
-        
-        const res2 = await supertest(serverAPI)
-        .put("/api/auth/users/1")
-        .set("authorization", res1.body.token)
+    it('200', () => {
+      return supertest(server).post('/api/auth/register')
         .send({
-            username: "Tatyana",
-            password: "123abc",
-            phone_number: "changed"
+          username: 'newuser',
+          phone_number: '2483804900',
+          password: 'pw',
         })
-    
-        expect(res2.statusCode).toBe(200);
-        expect(res2.body.phone_number).toBe("changed");
+        .expect(200);
     })
+  })
 
+  //LOGIN
 
-    // PLANT 
-
-    it("POST & GET /api/auth/plants/:id", async () => {
-        const res1 = await supertest(serverAPI).post("/api/auth/login").send({
-          username: "NewUser5",
-          password: "123abc"
-        });
-        
-        const res2 = await supertest(serverAPI).get("/api/auth/plants/1").set("authorization", res1.body.token)
-    
-        expect(res2.statusCode).toBe(200);
-        expect(res2.body).toBeDefined();
-      });
-    
-    
-      it("POST & POST /api/auth/plants", async () => { 
-        const res1 = await supertest(serverAPI).post("/api/auth/login").send({
-          username: "NewUser5",
-          password: "123abc"
-        });
-        
-        const res2 = await supertest(serverAPI).post("/api/auth/plants")
-        .set("authorization", res1.body.token)
-        .send({
-          nickname: "newname",
-          species: "newspecies",
-          h2oFrequency: "time",
-          plant_users_id: 1
+  describe('POST /api/auth/login', () => {
+    it('return JSON', async () => {
+      return supertest(server).post('/api/auth/login')
+        .then(res => {
+          expect(res.type).toMatch(/json/i)
         })
-    
-        expect(res2.statusCode).toBe(201);
-        expect(res2.body).toMatchObject({
-          id: 10,
-          nickname: "newname",
-          species: "newspecies",
-          h2oFrequency: "time",
-          plant_users_id: 1
-        });
-      });
-    
-    
-      it("POST  & PUT plants/:id", async () => {
-        const res1 = await supertest(serverAPI).post("/api/auth/login").send({
-          username: "Tatyana",
-          password: "123abc"
-        });
-        
-        const res2 = await supertest(serverAPI).put("/api/auth/plants/1")
-        .set("authorization", res1.body.token)
+    })
+    it('201', async () => {
+      res = await supertest(server)
+        .post('/api/auth/register')
         .send({
-          nickname: "changed",
-          species: "changed",
-          h2oFrequency: "changed",
-          plant_users_id: 1
-        })
-    
-        expect(res2.statusCode).toBe(200);
-        expect(res2.body).toMatchObject({
-          id: 1,
-          nickname: "changed",
-          species: "changed",
-          h2oFrequency: "changed",
-          plant_users_id: 1
+          username: 'alex',
+          phone_number: '98933212345',
+          password: '123t'
         });
-      });
-    
-    
-      it("POST & DELETE plants/:id", async () => {
-        const res1 = await supertest(serverAPI).post("/api/auth/login").send({
-          username: "NewUser5",
-          password: "123abc"
+      expect(res.status).toEqual(201);
+      res = await supertest(server)
+        .post('/api/auth/login')
+        .send({
+          username: 'alex',
+          password: '123t'
         });
-        
-        const res2 = await supertest(serverAPI).delete("/api/auth/plants/1") 
-    
-        expect(res2.statusCode).toBe(200);
-        expect(res2.body).toMatchObject({
-          id: 1,
-          nickname: "Croton1",
-          species: "Common Croton1",
-          h2oFrequency: "once a week1",
-          plant_users_id: 6
-        });
-      });
+      expect(res.status).toEqual(201);
+    })
+  })
+
 })
-
